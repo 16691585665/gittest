a61ccd36143a475abd58f87d1be202287f8a8046
gite <16691585665@.163.com>
2024-01-02 14:17:24 +0800

    first examination hemowork

--- src/main/java/com/huang/git/git.java
+++ src/main/java/com/huang/git/git.java
@@ -3,18 +3,12 @@ package com.huang.git;
 public class git {
 
     public static void main(String[] args){
-        System.out.println("hellow world");
-        System.out.println("hellow2");
-        System.out.println("hellow world33");
-        System.out.println("hellow world33");System.out.println("hellow world33");System.out.println("hellow world33");
-        System.out.println("hellow world33");
-        System.out.println("hellow world33");System.out.println("hellow world33");
-        System.out.println("hello ccaacava  afadafa");
+        System.out.println("liuqikai 22550202017");
+        System.out.println("liuqikai 22550202017");System.out.println("liuqikai 22550202017");
+        System.out.println("liuqikai 22550202017");
+        System.out.println("liuqikai 22550202017");
+        System.out.println("liuqikai 22550202017");
 
-        System.out.println("hello ccaacava  afadafa");
-        System.out.println("push  sdaadad");
-         System.out.println("pull");
-         System.out.println("pusll");
 
     }
 }
